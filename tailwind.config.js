34f36fc7700f5dc54f1f99a6ce09c76c6792bb2f
Jana Venice <148454980+janaveniice@users.noreply.github.com>
2025-02-24 18:25:25 +0800

    education section

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,13 @@ module.exports = {
         sans: ['Open Sans', 'sans-serif'],
       }
     },
+    screens: {
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    }
   },
   plugins: [],
 }
